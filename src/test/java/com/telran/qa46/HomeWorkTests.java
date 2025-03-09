a15e92a7681061d7411e829573817de09181cb52
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-03-09 10:54:14 +0200

    commit_09.03

---
+++ src/test/java/com/telran/qa46/HomeWorkTests.java
@@ -0,0 +1,134 @@
+package com.telran.qa46;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HomeWorkTests
+{
+    WebDriver driver;
+
+
+    @BeforeMethod
+    public void beforeMethod()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+         driver.get("https://demowebshop.tricentis.com/");
+    }
+
+    @Test
+    public void test()
+    {
+        System.out.println("The link was successfully opened");
+    }
+
+    @Test
+    public void findElementsWithSimpleLocators()
+    {
+//by tagname_1
+WebElement h3 = driver.findElement(By.tagName("h3"));
+System.out.println(h3.getText());
+
+//by tagname_2
+WebElement p = driver.findElement(By.tagName("p"));
+System.out.println(p.getText());
+
+//by class name_1
+driver.findElement(By.className("master-wrapper-page"));
+
+//by class name_2
+driver.findElement(By.className("footer"));
+
+//by ID_1
+driver.findElement(By.id("bar-notification"));
+
+//by ID_2
+driver.findElement(By.id("mob-menu-button"));
+
+//by linkText_1
+driver.findElement(By.linkText("Computers"));
+
+//by linkText_2
+driver.findElement(By.linkText("Books"));
+
+//by partial_linkText_1
+driver.findElement(By.partialLinkText("Apparel"));
+
+//by partial_linkText_2
+driver.findElement(By.partialLinkText("downloads"));
+
+}
+
+@Test
+public void cssSelectors()
+{
+//by tagname_1
+    WebElement h3 = driver.findElement(By.cssSelector("h3"));
+    System.out.println(h3.getText());
+
+//by tagname_2
+    WebElement p = driver.findElement(By.cssSelector("p"));
+    System.out.println(p.getText());
+
+//by class name_1
+    driver.findElement(By.cssSelector(".master-wrapper-page"));
+
+//by class name_2
+    driver.findElement(By.cssSelector(".footer"));
+
+//by ID_1
+    driver.findElement(By.cssSelector("#bar-notification"));
+
+//by ID_2
+    driver.findElement(By.cssSelector("#mob-menu-button"));
+
+//[attr='value']
+    driver.findElement(By.cssSelector("[href='/login']"));
+
+//CONTAINS -> *
+    driver.findElement(By.cssSelector("[href*='us']"));
+
+//start -> ^
+    driver.findElement(By.cssSelector("[href^='/about']"));
+
+//end -> $
+    driver.findElement(By.cssSelector("[href$='photo']"));
+
+}
+
+
+
+    @AfterMethod
+    public void afterMethod()
+    {
+    driver.quit();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
