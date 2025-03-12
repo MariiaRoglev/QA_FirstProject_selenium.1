84aaede7bd931ecb2fe8cf964394eac2bc1f9f49
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-03-12 19:58:11 +0200

    commit_12.03

---
+++ src/test/java/com/telran/qa46/XpathSelectorsHomeWork.java
@@ -0,0 +1,121 @@
+package com.telran.qa46;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class XpathSelectorsHomeWork
+{
+    WebDriver driver;
+
+    @BeforeMethod
+    public void before()
+    {
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demowebshop.tricentis.com/");
+
+    }
+
+    @AfterMethod
+    public void after()
+    {
+        driver.quit();
+    }
+
+
+//    @Test
+//    public void simpleSelectors()
+//    {
+//        WebElement h3 = driver.findElement(By.tagName("h3"));
+//        System.out.println(h3.getText());
+//
+//        driver.findElement(By.id("bar-notification"));
+//        driver.findElement(By.className("master-wrapper-page"));
+//        driver.findElement(By.linkText("Computers"));
+//        driver.findElement(By.partialLinkText("downloads"));
+//
+//    }
+//
+//    @Test
+//    public void cssSelectors()
+//    {
+//        WebElement h3 = driver.findElement(By.cssSelector("h3"));
+//        System.out.println(h3.getText());
+//
+//        driver.findElement(By.cssSelector("#bar-notification"));
+//        driver.findElement(By.cssSelector(".master-wrapper-page"));
+//        driver.findElement(By.cssSelector("[href='/computers']")); //Full text
+//        driver.findElement(By.cssSelector("[href^='/digital']")); //starts with
+//        driver.findElement(By.cssSelector("[href$='downloads']")); //ends with
+//        driver.findElement(By.cssSelector("[href*='downl']")); //contains
+//
+//        //tag + id
+//        driver.findElement(By.cssSelector("input#small-searchterms"));
+//
+//        //tag + class
+//        driver.findElement(By.cssSelector("div.search-box"));
+//
+//        //tag+ id+[attr='value']
+//        driver.findElement(By.cssSelector("input#small-searchterms[name='q']"));
+//    }
+
+    @Test
+    public void xpathSelectors()
+    {
+        //tag->xpath -> //"*"
+        driver.findElement(By.xpath("//img"));
+
+        //id-> xpath -> //*[@id='value']
+        // * = tag
+        driver.findElement(By.xpath("//div[@id='bar-notification']"));
+
+        //className -> xpath -> //*[@class='value']
+        driver.findElement(By.xpath("//div[@class='header-menu']"));
+
+        //Full text -> xpath -> //*[text()='FullText']
+        driver.findElement(By.xpath("//h3[text()='Information']")); //works the same: Text -> .
+        driver.findElement(By.xpath("//span[text()='Shopping cart']"));
+
+        //starts with -> //*[starts-with(@attr,'StartText')]
+        driver.findElement(By.xpath("//img[starts-with(@alt,'Tricentis')]"));
+        driver.findElement(By.xpath("//img[starts-with(@alt,'Picture')]"));
+
+        //contains -> xpath -> //*[contains(.,Text)]
+        driver.findElement(By.xpath("//img[contains(.,Web)]"));
+        driver.findElement(By.xpath("//img[contains(.,Demo)]"));
+        driver.findElement(By.xpath("//input[contains(.,Add)]"));
+
+        //move
+        driver.findElement(By.xpath("//div[@class='top-menu-triangle']")); //to find parent in the end -> /..
+        driver.findElement(By.xpath("//ul[@class='top-menu']"));
+
+        //parent
+        driver.findElement(By.xpath("//li/parent::*"));
+        driver.findElement(By.xpath("//li/parent::ul"));
+        driver.findElement(By.xpath("//li/.."));
+
+
+        //ancestor
+        driver.findElement(By.xpath("//span/ancestor::*")); //all
+        driver.findElement(By.xpath("//span/ancestor::form")); //all options
+        //driver.findElement(By.xpath("//input/ancestor::form[1]")); //to find only 1 option
+
+        //following-sibling
+        driver.findElement(By.xpath("//a/following-sibling::*"));
+        driver.findElement(By.xpath("//a/following-sibling::span"));
+
+        //preceding-sibling
+        driver.findElement(By.xpath("//span/preceding-sibling::*"));
+
+    }
+
+}
